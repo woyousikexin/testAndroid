1bd798c5f9b7a4ada52c1d5ada5047bf5c7c04fc
张运国 <zhangyunguo@chinapost.com.cn>
2023-11-24 15:06:39 +0800

    第一次提交

---
+++ app/src/main/java/com/example/testandroidongithub/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.testandroidongithub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "按钮被点击了...", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
